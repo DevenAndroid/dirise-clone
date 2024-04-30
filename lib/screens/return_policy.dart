492760d6b41f4126a95c3e62851b1b0b50a392ad
shivwp <shivkumawat1985@hotmail.com>
2024-04-30 17:44:36 +0530

    aayushi

--- lib/screens/return_policy.dart
+++ lib/screens/return_policy.dart
@@ -4,7 +4,8 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../language/app_strings.dart';
-enum BestTutorSite { javatpoint, w3schools, tutorialandexample }
+import '../widgets/common_colour.dart';
+import '../widgets/dimension_screen.dart';
 class ReturnnPolicy extends StatefulWidget {
   const ReturnnPolicy({super.key});
 
@@ -15,138 +16,243 @@ class ReturnnPolicy extends StatefulWidget {
 }
 
 class _ReturnnPolicyState extends State<ReturnnPolicy> {
+  int _radioValue1 = 1;
 
   @override
-  BestTutorSite _site = BestTutorSite.javatpoint;
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: GestureDetector(
-          onTap: () {
-            Get.back();
-          },
-          child: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_ios_new,
-              color: Color(0xff0D5877),
-              size: 16,
-            ),
-            onPressed: () {
-              // Handle back button press
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {
+              Get.back();
             },
+            child: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: Color(0xff0D5877),
+                size: 16,
+              ),
+              onPressed: () {
+                // Handle back button press
+              },
+            ),
+          ),
+          titleSpacing: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppStrings.returnnPolicy.tr,
+                style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+              ),
+            ],
           ),
         ),
-        titleSpacing: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              AppStrings.returnnPolicy.tr,
-              style: GoogleFonts.poppins(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20),
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: size.width,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 10),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: size.width,
 
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                border: Border.all(color: Color(0xffE4E2E2))),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    border: Border.all(color: Color(0xffE4E2E2))),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                    Text("Default:",
-                        style: GoogleFonts.poppins(
-                            fontSize: 20,
+                          children: [
+
+                            Text("My Default Policy:",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500)),
+
+                            Image(
+                                image: AssetImage(
+                                    "assets/icons/tempImageYRVRjh 1.png"))
+                          ],
+                        ),
+                      ),
+                      Divider(thickness: 1,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Return Within",    style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500)),
+                            Text("Return Shipping Fees",    style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500))
+                          ],
+                        ),
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  border: Border.all(color: Color(0xffD9D9D9)),
+                                  shape: BoxShape.rectangle),
+                              child: Text("14",    style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500)),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                                  border: Border.all(color: Color(0xffD9D9D9)),
+                                  shape: BoxShape.rectangle),
+                              child: Text("Days",    style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500)),
+                            ),
+                            Radio(
+                              value: 0,
+                              groupValue: _radioValue1,
+                              onChanged: (value) {
+                                setState(() {
+                                  _radioValue1 = value!;
+                                });
+                              },
+                            ),
+                            Text(
+                              'Buyer Pays',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500)
+                            ),
+                            Radio(
+                              // Visual Density passed here
+                              visualDensity: const VisualDensity(horizontal: -2.0),
+                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              value: 1,
+                              groupValue: _radioValue1,
+                              onChanged: (value) {
+                                setState(() {
+                                  _radioValue1 = value!;
+                                });
+                              },
+                            ),
+                            Text(
+                              'Seller Pays ',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500)
+                            ),
+                          ],
+                        ),
+                        Text("Return Policy Description",    style: GoogleFonts.poppins(
+                            fontSize: 16,
                             fontWeight: FontWeight.w500)),
+                        Text("Customer pay if he return the product because he made mistake or because he didn’t like the product.Vendor pay if the problem is from the product.",    style: GoogleFonts.poppins(
+                            fontSize:11,
+                            )),
+                        SizedBox(height: 13,),
+                        Row(
+                          children: [
+                            Text("Edit",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w300,
+                                    color: Color(0xff014E70))),
+                            Text("|Remove",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w300,
+                                    color: Color(0xff014E70))),
 
-                    Image(
-                        image: AssetImage(
-                            "assets/icons/tempImageYRVRjh 1.png"))
-                  ],
+                          ],
+                        ),
+                      ],),
+                    )
+                    ],
+                  ),
                 ),
-                Divider(thickness: 1,),
-                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                     Text("Return Within"),
-                     Text("Return Shipping Fees")
-                  ],
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-decoration: BoxDecoration(
-    border: Border.all(color: Color(0xffD9D9D9)),
-    shape: BoxShape.rectangle),
-                      child: Text("14"),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Color(0xffD9D9D9)),
-                          shape: BoxShape.rectangle),
-                      child: Text(
-                          "Days"),
-                    ),
-                    ListTile(
-                      title: const Text('www.javatpoint.com'),
-                      leading: Radio(
-                        value: BestTutorSite.javatpoint,
-                        groupValue: _site,
-                        // onChanged: (BestTutorSite value) {
-                        //   setState(() {
-                        //     _site = value;
-                        //   });
-                        // },
+
+
+              ),
+              SizedBox(height: 13,),
+              Row(mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("+Add New",style: TextStyle(decoration: TextDecoration.underline,color: Color(0xff014E70)
+                  ),),
+                ],
+              ),
+              SizedBox(height: 13,),
+              Text("DIRISE standard Policy",    style: GoogleFonts.poppins(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500)),
+              SizedBox(height: 13,),
+              Text("Policy 2",    style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500)),
+              Text("Policy 2",    style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500)),
+              Text("Policy 2",    style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500)),
+              SizedBox(height: 25,),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(ReturnnPolicy.route);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffF5F2F2),
+                      minimumSize: const Size(double.maxFinite, 60),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(AddSize.size5),
                       ),
-                    ),
-                    ListTile(
-                      title: const Text('www.w3school.com'),
-                      leading: Radio(
-                        value: BestTutorSite.w3schools,
-                        groupValue: _site,
-                        // onChanged: (BestTutorSite value) {
-                        //   setState(() {
-                        //     _site = value;
-                        //   });
-                        // },
+
+                      textStyle: GoogleFonts.poppins(
+                          fontSize: AddSize.font20, fontWeight: FontWeight.w600)),
+                  child: Text(
+                    "Save".tr,
+                    style: GoogleFonts.poppins(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: AddSize.font18),
+                  )),
+              SizedBox(height: 15,),
+              ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(ReturnnPolicy.route);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.maxFinite, 60),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(AddSize.size5),
                       ),
-                    ),
-                  ],
-                ),
-                Text("Return Policy Description"),
-                Text("Customer pay if he return the product because he made mistake or because he didn’t like the product.Vendor pay if the problem is from the product."),
-                Row(
-                  children: [
-                    Text("Edit",
-                        style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300,
-                            color: Color(0xff014E70))),
-                    Text("|Remove",
-                        style: GoogleFonts.poppins(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300,
-                            color: Color(0xff014E70))),
+                      backgroundColor: AppTheme.buttonColor,
+                      textStyle: GoogleFonts.poppins(
+                          fontSize: AddSize.font20, fontWeight: FontWeight.w600)),
+                  child: Text(
+                    "Skip".tr,
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: AddSize.font18),
+                  )),
 
-                  ],
-                ),
-              ],
-            ),
-
-
-          )
-        ],
+            ],
+          ),
         )
     );
   }
