42b9d7c42f858b4ea304e517935be7857ef3f718
EOXYS <dedeveneoxys@gmail.com>
2024-05-08 18:00:04 +0530

    piyush

--- lib/addNewProduct/optionalScreen.dart
+++ lib/addNewProduct/optionalScreen.dart
@@ -9,6 +9,7 @@ import 'package:form_field_validator/form_field_validator.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../controller/vendor_controllers/add_product_controller.dart';
 import '../model/common_modal.dart';
 import '../model/reviewAndPublishResponseodel.dart';
 import '../repository/repository.dart';
@@ -63,7 +64,7 @@ class _OptionalScreenState extends State<OptionalScreen> {
         String? SerialNumber;
         String? Productnumber;
 
-
+  final addProductController = Get.put(AddProductController());
 
   optionalApi() {
     Map<String, dynamic> map = {};
@@ -74,12 +75,13 @@ class _OptionalScreenState extends State<OptionalScreen> {
     map['long_description'] = longDescriptionController.text.trim();
     map['serial_number'] = serialNumberController.text.trim();
     map['product_number'] = productNumberController.text.trim();
+    map['id'] = addProductController.idProduct.value.toString();
 
     FocusManager.instance.primaryFocus!.unfocus();
     repositories.postApi(url: ApiUrls.giveawayProductAddress, context: context, mapData: map).then((value) {
       ReviewAndPublishResponseModel response = ReviewAndPublishResponseModel.fromJson(jsonDecode(value));
       print('API Response Status Code: ${response.status}');
-      showToast(response.message.toString());
+
       if (response.status == true) {
         productName = response.productDetails!.product!.pname.toString();
         productID = response.productDetails!.product!.id.toString();
@@ -297,8 +299,8 @@ class _OptionalScreenState extends State<OptionalScreen> {
                 const SizedBox(height: 20),
                 GestureDetector(
                   onTap: () {
-    if (formKey1.currentState!.validate()) {
-                    Get.to(ReviewPublishScreen());}
+
+                    Get.to(ReviewPublishScreen());
                   },
                   child: Container(
                     width: Get.width,
