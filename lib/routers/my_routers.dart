606a0549883e65cdbfa906a8b52a57fcd0f26c86
lavishmanghnani2 <139878421+lavishmanghnani2@users.noreply.github.com>
2024-05-15 16:02:10 +0530

    himpreet

--- lib/routers/my_routers.dart
+++ lib/routers/my_routers.dart
@@ -60,7 +60,7 @@ import '../vendor/shipping_policy.dart';
 class MyRouters {
   static var route = [
 
-    GetPage(name: '/', page: () => const TellUsYourSelfScreen()),
+    GetPage(name: '/', page: () => const BottomNavbar()),
     GetPage(name: LoginScreen.route, page: () => const LoginScreen()),
     GetPage(name: VendorOTPVerification.route, page: () => const VendorOTPVerification()),
     GetPage(name: CreateAccountNewScreen.route, page: () => const CreateAccountNewScreen()),
